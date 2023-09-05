969091a89f5bf5229aeea15bfa8c4e3adaede087
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-09-05 16:30:42 -0300

    Obtendo um usuário por id

--- src/main/java/com/enzocabrera/workshopmongo/services/UserService.java
+++ src/main/java/com/enzocabrera/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.enzocabrera.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enzocabrera.workshopmongo.domain.User;
 import com.enzocabrera.workshopmongo.repository.UserRepository;
+import com.enzocabrera.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,13 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+
 }
+
