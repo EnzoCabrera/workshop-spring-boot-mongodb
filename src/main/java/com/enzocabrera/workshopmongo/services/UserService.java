5aaf261fb42afcb1b87b75b59d461ee5df52d397
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-09-06 09:49:21 -0300

    Inserção de usuário com POST

--- src/main/java/com/enzocabrera/workshopmongo/services/UserService.java
+++ src/main/java/com/enzocabrera/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enzocabrera.workshopmongo.domain.User;
+import com.enzocabrera.workshopmongo.dto.UserDTO;
 import com.enzocabrera.workshopmongo.repository.UserRepository;
 import com.enzocabrera.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,7 +26,13 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
 
