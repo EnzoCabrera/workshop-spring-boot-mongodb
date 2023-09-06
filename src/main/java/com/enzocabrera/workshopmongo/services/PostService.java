a9cf778025ebd79fa84c3487e34d86441026bc8e
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-09-06 11:50:02 -0300

    Obtendo post por id

---
+++ src/main/java/com/enzocabrera/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.enzocabrera.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enzocabrera.workshopmongo.domain.Post;
+import com.enzocabrera.workshopmongo.repository.PostRepository;
+import com.enzocabrera.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
