659206854eaf511e14345bb6507e1f1740cfac7e
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-09-06 10:54:32 -0300

    Criando entity post com user aninhado

--- src/main/java/com/enzocabrera/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/enzocabrera/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,9 @@ package com.enzocabrera.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.enzocabrera.workshopmongo.domain.User;
-
-
+import com.enzocabrera.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface PostRepository extends MongoRepository<Post, String>{
 
 }
